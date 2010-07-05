d2394a4eacb9b48be8c88726da6897e41d740257
sdboyer <sdboyer>
2010-07-05 22:03:04 +0000

    Yowza; forgot to add the include call for cleanstring.

--- panels_ipe/plugins/display_renderers/panels_renderer_ipe.class.php
+++ panels_ipe/plugins/display_renderers/panels_renderer_ipe.class.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: panels_renderer_ipe.class.php,v 1.1.2.13 2010/07/05 21:55:35 sdboyer Exp $
+// $Id: panels_renderer_ipe.class.php,v 1.1.2.14 2010/07/05 22:03:04 sdboyer Exp $
 
 /**
  * Renderer class for all In-Place Editor (IPE) behavior.
@@ -85,6 +85,8 @@ class panels_renderer_ipe extends panels_renderer_editor {
     $output = parent::render_region($region_id, $panes);
     $output = theme('panels_ipe_region_wrapper', $output, $region_id, $this->display);
     $classes = 'panels-ipe-region';
+
+    ctools_include('cleanstring');
     $region_id = ctools_cleanstring($region_id);
     return "<div id='panels-ipe-regionid-$region_id' class='panels-ipe-region'>$output</div>";
   }
@@ -156,6 +158,8 @@ class panels_renderer_ipe extends panels_renderer_editor {
     else {
       $pane = $this->display->content[$pid];
     }
+
+    ctools_include('cleanstring');
     $region_id = ctools_cleanstring($pane->panel);
     $this->commands[] = ctools_ajax_command_append("#panels-ipe-regionid-$region_id div.panels-ipe-sort-container", $this->render_pane($pane));
 //    $this->commands[] = ctools_ajax_command_changed("#panel-pane-$pane->pid", "div.grabber span.text");
