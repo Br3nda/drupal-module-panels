4abd1a4eb691b655630baa301a6e3c4675c56613
merlinofchaos <merlinofchaos>
2010-08-21 20:46:36 +0000

    #696846: Mini panels conflated displayed title with admin title.

--- panels_mini/plugins/export_ui/panels_mini_ui.class.php
+++ panels_mini/plugins/export_ui/panels_mini_ui.class.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: panels_mini_ui.class.php,v 1.1.2.4 2010/07/29 20:02:26 merlinofchaos Exp $
+// $Id: panels_mini_ui.class.php,v 1.1.2.5 2010/08/21 20:46:36 merlinofchaos Exp $
 
 class panels_mini_ui extends ctools_export_ui {
   function init($plugin) {
@@ -66,7 +66,7 @@ class panels_mini_ui extends ctools_export_ui {
     // Set up sorting
     switch ($form_state['values']['order']) {
       case 'disabled':
-        $this->sorts[$item->name] = empty($item->disabled) . $item->title;
+        $this->sorts[$item->name] = empty($item->disabled) . $item->admin_title;
         break;
       case 'title':
         $this->sorts[$item->name] = $item->admin_title;
@@ -90,7 +90,7 @@ class panels_mini_ui extends ctools_export_ui {
 
     $this->rows[$item->name] = array(
       'data' => array(
-        array('data' => check_plain($item->title), 'class' => 'ctools-export-ui-title'),
+        array('data' => check_plain($item->admin_title), 'class' => 'ctools-export-ui-title'),
         array('data' => check_plain($item->name), 'class' => 'ctools-export-ui-name'),
         array('data' => $category, 'class' => 'ctools-export-ui-category'),
         array('data' => $layout, 'class' => 'ctools-export-ui-layout'),
